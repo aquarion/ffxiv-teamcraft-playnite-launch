0cda5236f3983612b5498fda84e2e8f5756253df
Nicholas Avenell <nicholas@istic.net>
2026-06-08 18:30:19 +0100

    Fix unused variable

--- teamcraft-launcher/1-Start.ps1
+++ teamcraft-launcher/1-Start.ps1
@@ -1,4 +1,4 @@
-if (![System.IO.File]::Exists($companionExe)){
+if (![System.IO.File]::Exists($companionExe)) {
   $PlayniteApi.Dialogs.ShowErrorMessage(("Failed to find Teamcraft at \r\n " + $companionExe), "FFXIV Teamcraft Launcher")
   exit 5
 }
@@ -8,25 +8,24 @@ $pleaseWait = 0 # How many times we've looped though waiting
 $launcherLoopWaitTime = 5 # How long to wait between checks
 $pleaseWaitPatienceSeconds = 120 # How long to wait before giving up (in seconds)
 
-$pleaseWaitPatience = $pleaseWaitPatienceSeconds/$launcherLoopWaitTime # How many times to loop before giving up (in loops)
+$pleaseWaitPatience = $pleaseWaitPatienceSeconds / $launcherLoopWaitTime # How many times to loop before giving up (in loops)
 
 
-while (!($launcher = Get-Process $gameExecutable -EA 0) -and ($pleaseWait -lt $pleaseWaitPatience ) ){
-      Start-Sleep -Seconds $launcherLoopWaitTime
-      $pleaseWait++
+while (!(Get-Process $gameExecutable -EA 0) -and ($pleaseWait -lt $pleaseWaitPatience ) ) {
+  Start-Sleep -Seconds $launcherLoopWaitTime
+  $pleaseWait++
 }
 
 # If we've waited too long, give up
-if ($pleaseWait -ge $pleaseWaitPatience){
-   $PlayniteApi.Dialogs.ShowErrorMessage("Got bored waiting for launcher", "FFXIV Teamcraft Launcher")
-   exit 200
+if ($pleaseWait -ge $pleaseWaitPatience) {
+  $PlayniteApi.Dialogs.ShowErrorMessage("Got bored waiting for launcher", "FFXIV Teamcraft Launcher")
+  exit 200
 }
 
 # ... then give the game some time to get its act together
 Start-Sleep -Seconds 10
 
 # Now launch Teamcraft if it isn't launched
-if (!(Get-Process -Name $companionName -EA 0))
-{
-    Start-Process -FilePath $companionExe
+if (!(Get-Process -Name $companionName -EA 0)) {
+  Start-Process -FilePath $companionExe
 }
